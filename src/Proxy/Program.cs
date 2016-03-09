8a92b71a437fab211748f3ae3d6ec919e864163f
jerusalemdax <jerusalemdax@gmail.com>
2016-03-10 03:27:05 +0800

    add project proxy

---
+++ src/Proxy/Program.cs
@@ -0,0 +1,9 @@
+﻿namespace Proxy
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+        }
+    }
+}
---
+++ src/Proxy/Properties/AssemblyInfo.cs
@@ -0,0 +1,23 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("Proxy")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("Proxy")]
+[assembly: AssemblyCopyright("Copyright ©  2016")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible
+// to COM components.  If you need to access a type in this assembly from
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("1496f5a7-587b-4ef9-b929-6fd6ded65b8a")]
